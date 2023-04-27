eccd82669516888555999060288b4b71a4bf3c8c
amaruAlavia <amarualavia@gmail.com>
2023-04-27 14:53:55 -0600

    V1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
+//Alerta de bienvenida
 alert("Hola Bienvenid@ a nuestro sitio web. No dudes en contactarnos ante cualquier duda.");
+
+//Smooth scroll
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+    if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+    scrollTop: $(hash).offset().top
+    }, 800, function(){
+    window.location.hash = hash;
+    });
+    }
+    });
+});
+
